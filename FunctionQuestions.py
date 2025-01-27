60a8dbb69e66b57cc331d468202f129691c98db2
Renato Pavlekovic <renato.pavlekovic92@gmail.com>
2025-01-27 16:12:29 +0000

    First Q

--- FunctionQuestions.py
+++ FunctionQuestions.py
@@ -1,3 +1,4 @@
+
 print("\nQ1a\n")
 # Q1a: Write a function which takes in an integer as an argument and returns the divisors of that number as a list
 # e.g. f(12) = [1, 2, 3, 4, 6, 12]
@@ -40,11 +41,16 @@ print(is_it_factor(int_1, int_2))
 
 print("\nQ2a\n")
 # Q2a: write a function which takes a letter (as a string) as an input and outputs it's position in the alphabet
-# alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-#             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " "]
 
 # A2a:
+letter = str(input("Enter a letter: "))
 
+def letter_position (letter):
+    return (alphabet.index(letter)+1)
+
+print(letter_position(letter))
 
 
 print("\nQ2b\n")
@@ -53,7 +59,19 @@ print("\nQ2b\n")
 # e.g. f("bob") = "1141" as "b" is in position 1 and "o" is in position 14
 
 # A2b:
+name = str(input("Enter a name: "))
 
+def generate_id_number (name):
+    list_of_letters = list(name)
+    id_number = []
+    joined_letters = ''
+    for i in range(len(list_of_letters)):
+        id_number.append(letter_position(list_of_letters[i]))
+    for j in id_number:
+        joined_letters += str(j)
+    return joined_letters
+
+print(generate_id_number(name))
 
 
 print("\nQ2c\n")
@@ -62,8 +80,14 @@ print("\nQ2c\n")
 # e.g. f("bob") -> 1134 (because bob's id was 1141 and 1+1+4+1 = 7 so 1141 - 7 = 1134)
 
 # A2c:
+def generate_password(id):
+    list_of_digits = list(id)
+    sum_digits = 0
+    for i in list_of_digits:
+        sum_digits += int(i)
+    return (int(id) - sum_digits)
 
-
+print('This is your password:', generate_password(generate_id_number(name)))
 
 # -------------------------------------------------------------------------------------- #
 
@@ -71,16 +95,32 @@ print("\nQ3a\n")
 # Q3a: Write a function which takes an integer as an input, and returns true if the number is prime, false otherwise.
 
 # A3a:
+def check_input(message):
+    while True:
+        try:
+            user_input_q3 = int(input(message))
+        except ValueError:
+            print("User input is not an integer. Try again.\n")
+            continue
+        else:
+            return user_input_q3
+            break
 
+def is_prime():
+    if len(find_divisors(user_input_q3)) > 2:
+        return True
+    else:
+        return False
 
+user_input_q3 = check_input("Enter a number: ")
+
+print(is_prime())
 
 print("\nQ3b\n")
 # Q3b: Now add some functionality to the function which does not error if the user inputs something other than a digit
 
 # A3b:
-
-
-
+#check the check_input function above
 # -------------------------------------------------------------------------------------- #
 
 
